8b81436796b714944d96b6a917a7738f0a2cfd0a
Roland Conybeare <rconybeare@gmail.com>
2025-09-22 12:53:48 -0400

    xo-jit: build: ignore llvm-owned maybe-uninitialized

--- include/xo/jit/IrPipeline.hpp
+++ include/xo/jit/IrPipeline.hpp
@@ -11,6 +11,7 @@
 /* stuff from kaleidoscope.cpp */
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
 # include "llvm/ADT/APFloat.h"
 # include "llvm/ADT/STLExtras.h"
 # include "llvm/IR/BasicBlock.h"
