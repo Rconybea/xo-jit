50b0f7698c7ac6357b49e2a5efcae19f79c492e4
Roland Conybeare <rconybeare@gmail.com>
2025-11-29 16:59:36 -0500

    xo-alloc: + ArenaAllocT for use with std::map() etc.

---
+++ utest/ArenaAllocT.test.cpp
@@ -0,0 +1,65 @@
+/** @file ArenaAllocT.test.cpp
+ *
+ *  @author Roland Conybeare, Nov 2025
+ **/
+
+#include "xo/alloc/ArenaAllocT.hpp"
+#include <catch2/catch.hpp>
+#include <map>
+
+namespace xo {
+    using xo::gc::ArenaAllocT;
+    using xo::gc::ArenaAlloc;
+
+    namespace ut {
+
+        namespace {
+            struct testcase_ArenaAllocT {
+                testcase_ArenaAllocT(std::size_t z) : arena_z_{z} {}
+
+                std::size_t arena_z_;
+                std::vector<std::pair<std::string, std::string>> kv_pairs_;
+            };
+
+            std::vector<testcase_ArenaAllocT>
+            s_testcase_v = {
+                testcase_ArenaAllocT(4096)
+            };
+        }
+
+        TEST_CASE("ArenaAllocT", "[alloc][traits]")
+        {
+            for (std::size_t i_tc = 0, n_tc = s_testcase_v.size(); i_tc < n_tc; ++i_tc) {
+                const testcase_ArenaAllocT & tc = s_testcase_v[i_tc];
+
+                constexpr bool c_debug_flag = true;
+
+                auto arena = ArenaAlloc::make("arena", tc.arena_z_, c_debug_flag);
+                auto alloc = ArenaAllocT<std::pair<const std::string, std::string>>(arena.get());
+
+                using TestMapType = std::map<std::string,
+                                             std::string,
+                                             std::less<std::string>,
+                                             ArenaAllocT<std::pair<const std::string, std::string>>>;
+
+                TestMapType test_map(alloc);
+
+                size_t n = 0;
+                for (const auto & kv_ix : tc.kv_pairs_) {
+                    test_map[kv_ix.first] = kv_ix.second;
+                    ++n;
+
+                    REQUIRE(test_map.size() == n);
+
+                    for (const auto & map_ix : test_map) {
+                        map_ix.first;
+                        map_ix.second;
+                    }
+                }
+
+            }
+        }
+    }
+} /*namespace xo*/
+
+/* end ArenaAllocT.test.cpp */
