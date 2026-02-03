0fff79d6b702522adb0e8e609df5f75f8838ccaf
Roland Conybeare <rconybeare@gmail.com>
2026-02-03 14:27:42 -0500

    xo-reader2 stack: streamlining + arith parser test

---
+++ include/xo/expression2/ApplyExpr.hpp
@@ -0,0 +1,13 @@
+/** @file ApplyExpr.hpp
+ *
+ *  @author Roland Conybeare, Feb 2026
+ **/
+
+#pragma once
+
+#include "DApplyExpr.hpp"
+#include "detail/IExpression_DApplyExpr.hpp"
+//#include "detail/IGCObject_DApplyExpr.hpp"
+#include "detail/IPrintable_DApplyExpr.hpp"
+
+/* end ApplyExpr.hpp */
