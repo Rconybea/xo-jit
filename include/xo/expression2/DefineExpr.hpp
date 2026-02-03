523538b21ce69f2eeb8a8b9e16f991f01d9a6c32
Roland Conybeare <rconybeare@gmail.com>
2026-02-03 13:43:00 -0500

    xo-reader2 stack: convenience #includes + parsing examples

---
+++ include/xo/expression2/DefineExpr.hpp
@@ -0,0 +1,13 @@
+/** @file DefineExpr.hpp
+ *
+ *  @author Roland Conybeare, Feb 2026
+ **/
+
+#pragma once
+
+#include "DDefineExpr.hpp"
+#include "detail/IExpression_DDefineExpr.hpp"
+//#include "detail/IGCObject_DDefineExpr.hpp"
+#include "detail/IPrintable_DDefineExpr.hpp"
+
+/* end DefineExpr.hpp */
