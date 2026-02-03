f77a94af6fd5be8526426fd92035865fe8558f22
Roland Conybeare <rconybeare@gmail.com>
2026-02-03 13:23:38 -0500

    xo-reader2 stack: misc qol improvements + improve reader2 utest

---
+++ include/xo/expression2/Constant.hpp
@@ -0,0 +1,13 @@
+/** @file Constant.hpp
+ *
+ *  @author Roland Conybeare, Feb 2026
+ **/
+
+#pragma once
+
+#include "DConstant.hpp"
+#include "detail/IExpression_DConstant.hpp"
+#include "detail/IGCObject_DConstant.hpp"
+#include "detail/IPrintable_DConstant.hpp"
+
+/* end Constant.hpp */
