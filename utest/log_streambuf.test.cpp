d60b74d2b52b3ab878642bf0d069f2bd5b255137
Roland Conybeare <rconybeare@gmail.com>
2026-03-17 12:24:27 -0400

    xo-indentlog: utest: carveout for osx-vs-linux nullptr printing

--- utest/log_streambuf.test.cpp
+++ utest/log_streambuf.test.cpp
@@ -77,7 +77,11 @@ namespace ut {
 
         //REQUIRE(sbuf.lo() == sbuf.pbase());
 
+#ifdef __linux__
+        auto expected = string_view("empty log_streambuf :sbuf.lo 0 :sbuf.hi 0 :sbuf.color_escape_chars 0");
+#else        
         auto expected = string_view("empty log_streambuf :sbuf.lo 0x0 :sbuf.hi 0x0 :sbuf.color_escape_chars 0");
+#endif
 
         REQUIRE(string_view(sbuf) == expected);
 
