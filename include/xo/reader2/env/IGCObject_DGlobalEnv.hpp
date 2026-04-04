6251b2bde7682b3f1e0f210234e7d9b7f39160f6
Roland Conybeare <rconybeare@gmail.com>
2026-04-04 16:54:46 -0400

    refactor: void return type for Collector.forward_children()

--- include/xo/reader2/env/IGCObject_DGlobalEnv.hpp
+++ include/xo/reader2/env/IGCObject_DGlobalEnv.hpp
@@ -57,7 +57,7 @@ namespace xo {
             /** move instance using allocator **/
             static Opaque shallow_move(DGlobalEnv & self, obj<ACollector> gc) noexcept;
             /** during GC: forward immdiate children **/
-            static size_type forward_children(DGlobalEnv & self, obj<ACollector> gc) noexcept;
+            static void forward_children(DGlobalEnv & self, obj<ACollector> gc) noexcept;
             ///@}
         };
 
--- src/reader2/facet/IGCObject_DGlobalEnv.cpp
+++ src/reader2/facet/IGCObject_DGlobalEnv.cpp
@@ -27,9 +27,9 @@ namespace xo {
             return self.shallow_move(gc);
         }
         auto
-        IGCObject_DGlobalEnv::forward_children(DGlobalEnv & self, obj<ACollector> gc) noexcept -> size_type
+        IGCObject_DGlobalEnv::forward_children(DGlobalEnv & self, obj<ACollector> gc) noexcept -> void
         {
-            return self.forward_children(gc);
+            self.forward_children(gc);
         }
 
     } /*namespace scm*/
